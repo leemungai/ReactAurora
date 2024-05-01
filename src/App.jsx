705a016380cdffc1314baa7a3b99539eff77efd9
leemungai <mrmungai@yahoo.com>
2024-05-01 21:26:18 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react'
+
+function App() {
+
+  return (
+    <div className='to-do-list'>
+      <h1>To-Do-Li</h1>
+
+    </div>
+  )
+}
+
+export default App;
+
